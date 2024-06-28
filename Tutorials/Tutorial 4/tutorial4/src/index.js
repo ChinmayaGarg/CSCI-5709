149fe7ea73b34b17398c4fadea5f0ffbb2e139dc
Chinmaya Garg <ch745692@dal.ca>
2024-06-28 10:24:12 +0530

    Code Updated

--- Tutorials/Tutorial 4/tutorial4/src/index.js
+++ Tutorials/Tutorial 4/tutorial4/src/index.js
@@ -3,19 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import LoginPage from './pages/LoginPage';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     {/* <Router> */}
-//     <App />
-//     {/* </Router> */}
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<App />);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
